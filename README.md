e512a409430c74b642dcfca0bfeaaa576df34f76
jacksongeller <jacksongeller@gmail.com>
2014-09-12 15:52:31 -0400

    use

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 in development
+
+
+### Use
+
+```js
+var fs = require('fs');
+var lodashStreamer = require('lodash-template-stream');
+
+var read = fs.createReadStream('./template.html');
+
+read
+  .pipe(lodashStreamer({my:'obj'}))
+  .pipe(process.stdout);
+
+```
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var _ = require('lodash');
 var through = require('through2');
 
+module.exports = function(object) {
+  through2(function(chunk, enc, done) {    
+    var compiled = _.template(chunk, object);
+    this.push(compiled)
+    return done();    
+  }));
+}
