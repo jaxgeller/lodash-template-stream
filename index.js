ec2cf697fb41b80bf2520242ff4b8f05b2261b1d
jacksongeller <jacksongeller@gmail.com>
2014-09-12 15:48:14 -0400

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+var _ = require('lodash');
+var through = require('through2');
+
---
+++ test/index.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('streamer', function() {
+  
+  it('should work', function(done) {
+    done();
+  });
+
+})
